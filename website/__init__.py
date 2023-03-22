e7b64654ad3255afe1bd916a51dce0ee9e23a90a
Joshua Astle <jovahkin259@gmail.com>
2023-03-22 21:49:11 +0000

    Add blueprint for views

--- website/__init__.py
+++ website/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from .views import views
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "secretkey"
-
+    app.register_blueprint(views, url_prefix="/")
     return app
